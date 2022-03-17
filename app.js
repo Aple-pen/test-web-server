a716d2431c06c57e812a03323b6c2d972d60965d
eunggikim <eunggi3374>
2022-03-17 16:57:06 +0900

    get customer api 작성

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express")
+require('dotenv').config({path:'mysql/.env'})
+const mysql = require("./mysql")
 const app = express();
 const PORT = 3000
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json({
+    limit : '50mb'
+}))
 
 app.get("/",(req,res)=>{
     res.send('Hello World~')
 })
 
+app.get("/api/customers",async(req,res)=>{
+    const customers = await mysql.query("customerList")
+    console.log(customers)
+    res.send(customers)
+})
+
+app.post("/api/customer/insert",async(req,res)=>{
+    const result = await mysql.query('customerInsert',req.body.param);
+    console.log(result)
+    res.send(result);
+})
+
 app.listen(PORT,()=>{
     console.log("server running~")
+
 })
